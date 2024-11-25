5b09a02d30a10db38b18cc096760323acddfc0cd
Nakano as a Service <nakanoasaservice@gmail.com>
2024-11-25 12:39:42 +0900

    feat: t3-envを導入

---
+++ apps/web/env.ts
@@ -0,0 +1,26 @@
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
+
+export const env = createEnv({
+  /*
+   * サーバーサイドの環境変数です。クライアントでは使用できません。
+   * クライアントでこれらの変数にアクセスすると例外がスローされます。
+   */
+  server: {},
+  /*
+   * クライアント（およびサーバー）で使用可能な環境変数です。
+   *
+   * 💡 これらの変数の前にNEXT_PUBLIC_を付けないと、型エラーが発生します。
+   */
+  client: {},
+  /*
+   * Next.jsはEdgeとClientの環境変数をバンドルするため、
+   * すべての環境変数がバンドルされるように、手動で環境変数を再構築する必要があります。
+   *
+   * 💡 `server` と `client` のすべての変数がここに含まれていないと、型エラーが発生します。
+   */
+  runtimeEnv: {
+    // server
+    // client
+  },
+})
