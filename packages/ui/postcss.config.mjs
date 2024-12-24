29c21829caeb181423655e6d5f56824d63a91178
hyodoblog <yusei.hyodo@gmail.com>
2024-12-25 00:33:30 +0900

    lint error fix.

--- packages/ui/postcss.config.mjs
+++ packages/ui/postcss.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    tailwindcss: {
+      //
+    },
+    autoprefixer: {
+      //
+    },
   },
 }
 
